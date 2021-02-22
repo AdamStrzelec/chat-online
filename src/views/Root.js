36c8fa344aab3e89cd6db37bb90007485d78c021
adastr96 <adam19960@op.pl>
2021-02-22 01:01:41 +0100

    firebase configuration done

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Root = () => (
-    <>
-        
-    </>
-)
+const Root = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
 
 export default Root;
